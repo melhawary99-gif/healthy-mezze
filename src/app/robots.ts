d7d5ce6576ba795841cd33809661d7d664f49e66
melhawary99-gif <melhawary99@gmail.com>
2026-08-08 23:40:03 +0600

    fix: use canonical www domain in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
+    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
